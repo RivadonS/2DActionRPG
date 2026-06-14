53b31c124ec1d82b3e10b5fb9390886983a4cf16
RivadonS <rivadon.sung@bumail.net>
2026-06-14 17:23:52 +0700

    Idle, Walk, Turn Around Animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,22 +6,28 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement Settings")]
     [SerializeField] private float speed = 5f;
+    private int facingDirection = 1; // 1 for right, -1 for left
 
-    [Header("Input")]
+    [Header("Input & Animation")]
     [SerializeField] private InputActionReference moveActionReference;
+    [SerializeField] private Animator anim;
 
     public Rigidbody2D rb;
     private Vector2 movementInput;
+
+    private readonly int animHorizontal = Animator.StringToHash("horizontal");
+    private readonly int animVertical = Animator.StringToHash("vertical");
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 0f;
+
+        if (anim == null) anim = GetComponent<Animator>();
     }
 
     private void OnEnable()
     {
-        // ต้องเรียกผ่าน .action 
         if (moveActionReference != null)
         {
             moveActionReference.action.Enable();
@@ -42,10 +48,37 @@ public class PlayerMovement : MonoBehaviour
         {
             movementInput = moveActionReference.action.ReadValue<Vector2>();
         }
+
+        UpdateAnimation();
     }
 
     void FixedUpdate()
     {
         rb.linearVelocity = movementInput.normalized * speed;
+
+        CheckFlip();
+    }
+
+    private void UpdateAnimation()
+    {
+        anim.SetFloat(animHorizontal, Mathf.Abs(movementInput.x));
+        anim.SetFloat(animVertical, Mathf.Abs(movementInput.y));
+    }
+
+    private void CheckFlip()
+    {
+        if (movementInput.x > 0 && transform.localScale.x < 0 ||
+            movementInput.x < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        facingDirection *= -1;
+        Vector3 newScale = transform.localScale;
+        newScale.x *= -1;
+        transform.localScale = newScale;
     }
 }
